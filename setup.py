45127020ecad7d8eb55929032fa1db3ba5fb4ce6
Jiajun <jiajunb@umich.edu>
2020-03-21 00:11:43 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='EECS 476 Final Project',
+    author='eecs476team',
+    license='MIT',
+)
