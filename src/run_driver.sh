dcc739fae8ca4612beaa38f06b803383e11c62a4
Jiajun <jiajunb@umich.edu>
2020-04-18 22:50:48 -0400

    albert

--- src/run_driver.sh
+++ src/run_driver.sh
@@ -1,7 +1,7 @@
 python albert_driver.py
---input_dir data/processed/balanced
---output_dir checkpoints/albert/
---epochs 5
---learning_rate 1e-5
---per_gpu_batch_size 64
+--input_dir data/processed/balanced \
+--output_dir checkpoints/albert/ \
+--epochs 5 \
+--learning_rate 1e-5 \
+--per_gpu_batch_size 64 \
 --comment albert
