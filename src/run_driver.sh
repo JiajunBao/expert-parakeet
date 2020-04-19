df63643ff33e63c99aeb17e6fa84202ac0ad220a
Jiajun <jiajunb@umich.edu>
2020-04-19 12:25:34 -0400

    albert change batch size

--- src/run_driver.sh
+++ src/run_driver.sh
@@ -1,5 +1,5 @@
 /opt/anaconda/bin/python3 src/albert_driver.py \
---input_dir data/processed/balanced \
+--input_dir data/processed/full \
 --output_dir checkpoints/albert/ \
 --epochs 5 \
 --learning_rate 1e-5 \
