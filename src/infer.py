5b476068c70d58a87c755d15196448574728a041
Jiajun <jiajunb@umich.edu>
2020-04-19 16:34:55 -0400

    albert change batch size

--- src/infer.py
+++ src/infer.py
@@ -8,14 +8,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-
-checkpoints = torch.load('checkpoints/albert/best_model.pth.tar')
+# item()
+checkpoints = torch.load('checkpoints/albert_balanced/best_model.pth.tar')
 model = AlbertForReviewClassification.from_pretrained('albert-base-v2', num_labels=8)
 model.load_state_dict(checkpoints["model"])
-args = torch.load('checkpoints/albert/best_trainer.pth.tar')['args']
+args = torch.load('checkpoints/albert_balanced/best_trainer.pth.tar')['args']
 logger.info("model load completed")
 trainer = Solver(args, model)
-preds, golds, _, _ = trainer.infer(Path('data/processed/full/test'))
-torch.save(preds, 'checkpoints/albert/preds.pth')
-torch.save(golds, 'checkpoints/albert/golds.pth')
+preds, golds, _, _ = trainer.infer(Path('data/processed/balanced/test'))
+torch.save(preds, 'checkpoints/albert_balanced/preds.pth')
+torch.save(golds, 'checkpoints/albert_balanced/golds.pth')
 
